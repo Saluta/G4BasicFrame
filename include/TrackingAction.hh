9165fe0b0a3af6b21694fff10c50132e5c17a7c5
Saluta.Hikari <saluta.hikari@gmail.com>
2023-11-19 16:40:25 +0800

    Update TrackingAction.hh
    Dump unnecessary content of TrackingAction.hh

--- include/TrackingAction.hh
+++ include/TrackingAction.hh
@@ -1,7 +1,6 @@
 #ifndef TrackingAction_HH
 #define TrackingAction_HH 1
 
-#include "globals.hh"
 #include "G4UserTrackingAction.hh"
 
 class G4Track;
@@ -14,11 +13,6 @@ class TrackingAction : public G4UserTrackingAction
 
     virtual void  PreUserTrackingAction(const G4Track*);
     virtual void PostUserTrackingAction(const G4Track*);
-
-    void AddEdep(G4double addE) {fEdep += addE;}
-
-private:
-    G4double fEdep;
 };
 
 
