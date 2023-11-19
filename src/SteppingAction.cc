2b4db7f25774ca813d4a164c4219512de941d994
Saluta.Hikari <saluta.hikari@gmail.com>
2023-11-19 16:47:09 +0800

    Update SteppingAction.cc
    Dump unnecessary content of SteppingAction.cc

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -1,21 +1,9 @@
 #include "SteppingAction.hh"
-#include "RunAction.hh"
-#include "EventAction.hh"
-#include "TrackingAction.hh"
-#include "Analysis.hh"
 
-#include "G4SystemOfUnits.hh"
-#include "G4EventManager.hh"
 #include "G4Step.hh"
-#include "G4StepPoint.hh"
-#include "G4Track.hh"
-#include "G4TrackStatus.hh"
-#include "G4VProcess.hh"
 
-
-SteppingAction::SteppingAction(EventAction * eveAct)
- : G4UserSteppingAction(),
-   fEventAction(eveAct)
+SteppingAction::SteppingAction()
+ : G4UserSteppingAction()
 {
 
 }
@@ -26,17 +14,5 @@ SteppingAction::~SteppingAction()
 
 void SteppingAction::UserSteppingAction(const G4Step* step)
 {
-    G4VPhysicalVolume * prePointPV =
-            step->GetPreStepPoint()->GetTouchableHandle()->GetVolume();
 
-    if(prePointPV == nullptr)
-    {
-        return;
-    }
-
-    G4String stepPVName = prePointPV->GetName();
-    if(stepPVName == "physDetCore")
-    {
-        fEventAction->AddEdep(step->GetTotalEnergyDeposit());
-    }
 }
