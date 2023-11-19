efc0fe2edc43d8d60837909d5309d5200b391cbb
Saluta.Hikari <saluta.hikari@gmail.com>
2023-11-19 16:41:17 +0800

    Update TrackingAction.cc
    Dump unnecessary content of TrackingAction.cc

--- src/TrackingAction.cc
+++ src/TrackingAction.cc
@@ -1,15 +1,9 @@
 #include "TrackingAction.hh"
-#include "Analysis.hh"
 
 #include "G4Track.hh"
-#include "G4TrackingManager.hh"
-#include "G4VProcess.hh"
-#include "G4SystemOfUnits.hh"
-
 
 TrackingAction::TrackingAction()
- : G4UserTrackingAction(),
-   fEdep(0.)
+ : G4UserTrackingAction()
 {
 }
 
@@ -20,14 +14,10 @@ TrackingAction::~TrackingAction()
 
 void TrackingAction::PreUserTrackingAction(const G4Track * )
 {
-    fEdep = 0;
+
 }
 
 void TrackingAction::PostUserTrackingAction(const G4Track * )
 {
-//    // fill the collected energy deposition to the histogram (spectrum)
-//    G4AnalysisManager * analysisManager = G4AnalysisManager::Instance();
 
-//    G4int EspecID = analysisManager->GetH1Id("RingESpec");
-//    analysisManager->FillH1(EspecID, fEdep);
 }
